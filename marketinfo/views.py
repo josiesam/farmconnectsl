7d6f4c0808e93a5aed29844788eee3aaa4f1291e
josiesam <josiesam369@gmail.com>
2023-11-28 17:32:48 +0000

    Create view for marketinfo

--- marketinfo/views.py
+++ marketinfo/views.py
@@ -69,6 +69,87 @@ class CropDeleteView(DeleteView):
     template_name = 'crop/crop_confirm_delete.html'
     success_url = reverse_lazy('crop_list')
 
+class MarketpriceListView(ListView):
+    model = MarketPrice
+    template_name = 'marketprice/marketprice_list.html'
+    context_object_name = 'marketprices'
+
+
+
+class MarketPriceDetailView(DetailView):
+    model = MarketPrice
+    template_name = 'marketprice/marketprice_detail.html'
+    context_object_name = 'marketprice'
+
+
+class MarketPriceCreateView(CreateView):
+    model = MarketPrice
+    template_name = 'marketprice/marketprice_form.html'
+    form_class = MarketPriceForm
+    success_url = reverse_lazy('marketprice_list')
+
+
+class MarketPriceUpdateView(UpdateView):
+    model = MarketPrice
+    template_name = 'marketprice/marketprice_form.html'
+    form_class = MarketPriceForm
+    success_url = reverse_lazy('marketprice_list')
+
+
+class MarketPriceDeleteView(DeleteView):
+    model = MarketPrice
+    template_name = 'marketPrice/marketprice_confirm_delete.html'
+    success_url = reverse_lazy('marketPrice_list')
 
 # Repeat the above pattern for other models (MarketPrice, Event, Attendee, BlogPost, Comment, UserSubmittedArticle)
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'event/event_detail.html'
+    context_object_name = 'event'
+
+
+class EventCreateView(CreateView):
+    model = Event
+    template_name = 'event/event_form.html'
+    form_class = EventForm
+    success_url = reverse_lazy('event_list')
+
+
+class EventUpdateView(UpdateView):
+    model = Event
+    template_name = 'event/event_form.html'
+    form_class = EventForm
+    success_url = reverse_lazy('event_list')
+
+
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = 'event/event_confirm_delete.html'
+    form_class = EventForm
+    success_url = reverse_lazy('event_list')
+    context_object_name = 'event'
+
+
+class BlogPostView(CreateView):
+    model = BlogPost
+    template_name = 'blogpost/blogpost_form.html'
+    form_class = BlogPostForm
+    success_url = reverse_lazy('blogpost_list')
+
+
+class BlogPostView(UpdateView):
+    model = BlogPost
+    template_name = 'blogpost/blogpost_form.html'
+    form_class = BlogPostForm
+    success_url = reverse_lazy('blogpost_list')
+
+
+class BlogPostView(BlogPostView):
+    model = BlogPost
+    template_name = 'blogpost/blogpost_confirm_delete.html'
+    form_class = BlogPostForm
+    success_url = reverse_lazy('blogpost_list')
+
+
 
